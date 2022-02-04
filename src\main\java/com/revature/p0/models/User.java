473c91692ff878bfead69dda5d028cc3711ccab6
Hannah Hospital <delopitalhannah@gmail.com>
2022-02-03 18:23:10 -0600

    Updated the User model

--- src\main\java/com/revature/p0/models/User.java
+++ src\main\java/com/revature/p0/models/User.java
@@ -1,5 +1,25 @@
 package com.revature.p0.models;
 
 public class User {
+	
+	private String type;
+	private String name;
+	
+	public User(String type, String name) {
+		super();
+		this.type = type;
+		this.name = name;
+	}
 
+	public String getType() {
+		return type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	
+	
 }
